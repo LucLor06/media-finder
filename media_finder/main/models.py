6454fb2cb9c1ddd72d10f27adf1c3565b23f2010
LucLor06 <LukeLorenzBA@gmail.com>
2024-12-21 11:18:49 -0500

    Create Genre, Book, Movie, and Show models

--- media_finder/main/models.py
+++ media_finder/main/models.py
@@ -4,3 +4,22 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     ...
+
+class Genre(models.Model):
+    name = models.CharField(max_length=32)
+
+class AbstractMedia(models.Model):
+    name = models.CharField(max_length=64)
+    
+    class Meta:
+        abstract = True
+        
+class Book(AbstractMedia):
+    genres = models.ManyToManyField('Genre', related_name='books')
+    
+class Movie(AbstractMedia):
+    genres = models.ManyToManyField('Genre', related_name='movies')
+    
+class Show(AbstractMedia):
+    genres = models.ManyToManyField('Genre', related_name='shows')
+    
